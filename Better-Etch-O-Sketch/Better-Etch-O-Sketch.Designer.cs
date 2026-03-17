6cfc261ffc707efc2f8b99a61d85ffd70c6bc93f
micahspencer-png <micahspencer@isu.edu>
2026-03-17 11:31:44 -0600

    Add project files.

---
+++ Better-Etch-O-Sketch/Better-Etch-O-Sketch.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Better_Etch_O_Sketch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.PortComboBox = new System.Windows.Forms.ComboBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(679, 387);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(109, 51);
+            this.ExitButton.TabIndex = 0;
+            this.ExitButton.Text = "E&xit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // PortComboBox
+            // 
+            this.PortComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PortComboBox.FormattingEnabled = true;
+            this.PortComboBox.Location = new System.Drawing.Point(700, 73);
+            this.PortComboBox.Name = "PortComboBox";
+            this.PortComboBox.Size = new System.Drawing.Size(88, 33);
+            this.PortComboBox.TabIndex = 1;
+            this.PortComboBox.SelectedIndexChanged += new System.EventHandler(this.PortComboBox_SelectedIndexChanged);
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(576, 387);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(100, 50);
+            this.ConnectButton.TabIndex = 2;
+            this.ConnectButton.Text = "&Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.PortComboBox);
+            this.Controls.Add(this.ExitButton);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Better Etch-O-Sketch";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.ComboBox PortComboBox;
+        private System.Windows.Forms.Button ConnectButton;
+    }
+}
+
---
+++ Better-Etch-O-Sketch/Better-Etch-O-Sketch.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading;
+//using System.Threading.
+using System.Windows.Forms;
+
+namespace Better_Etch_O_Sketch
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort1 = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+            PortCombo();
+        }
+
+        //Program Logic----------------------------------------------------------------------------------------------------------------
+        void PortCombo()
+        {
+            PortComboBox.Items.Clear();
+            for (int num = 1; num < 9; num++)
+            {
+                PortComboBox.Items.Add($"COM{num}");
+            }
+        }
+
+        void SerialConnect()
+        {
+            serialPort1.Close();
+            string port = PortComboBox.Text;
+            serialPort1.BaudRate = 9600;
+            serialPort1.PortName = port;
+            serialPort1.Parity = System.IO.Ports.Parity.None;
+            //serialPort1.StopBits = System.IO.Ports.StopBits.None;
+            try
+            {
+                serialPort1.Open();
+                if (serialPort1.IsOpen)
+                {
+                    MessageBox.Show($"{serialPort1.PortName} Connected Successfully");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            IsQYAtBoardCheck();
+            //ConnectButton.Enabled = false;
+        }
+        byte[] SendData(byte[] data)
+        {
+            byte[] buffer = new byte[0];
+            if (serialPort1.IsOpen)
+            {
+                //flush old bytes from buffers
+                serialPort1.DiscardInBuffer();
+                serialPort1.DiscardOutBuffer();
+
+                //send command
+                serialPort1.Write(data, 0, data.Length);
+
+                //wait for response
+                Thread.Sleep(100);
+                //make the array the size of the input buffer
+                buffer = new byte[serialPort1.BytesToRead];
+                //actually read the input buffer
+                serialPort1.Read(buffer, 0, buffer.Length);
+            }
+            return buffer;
+        }
+
+        bool IsQYAtBoardCheck()
+        {
+            bool QYAt = false;
+            byte[] QyAtSettings = { 0xf0 };
+            byte[] ReadBuffer = SendData(QyAtSettings);
+            if (ReadBuffer.Length == 64 && ReadBuffer[58] == 81 && ReadBuffer[59] == 121 && ReadBuffer[60] == 64)
+            {
+                MessageBox.Show($"QY@ Board Connected  to {serialPort1.PortName}");
+                QYAt = true;
+            }
+            return QYAt;
+        }
+
+        void WriteDigital(byte pins)
+        {
+            byte[] data = { 0x20, pins};
+            SendData(data);
+        }
+
+        byte[] ReadAnalog(byte analogInputs)
+        {
+            byte command = 0x50;
+            byte[] data = {command};
+            byte[] response;
+            int highByte;
+            int lowByte;
+            int value;
+            analogInputs = (byte)(0x0F & analogInputs);
+            command = (byte)(command | analogInputs);
+            //Console.WriteLine($"{command:X2}");
+
+            data[0] = command;
+            while (true)
+            {
+                response = SendData(data);
+                //foreach (byte b in response)
+                //{
+                //    Console.Write($"{b} ");
+                //}
+                //Console.WriteLine();
+                highByte = response[0] << 2;
+                lowByte = response[1] >> 6;
+                value = highByte + lowByte;
+                Console.WriteLine(value);
+            }
+            return response;
+        }
+
+        //Event Handler--------------------------------------------------------------------------------------------------------------
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if (PortComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Select Port");
+            }
+            else
+            {
+                SerialConnect();
+            }
+            ReadAnalog(0x02);
+        }
+
+        private void PortComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ConnectButton.Enabled = true;
+        }
+    }
+}
