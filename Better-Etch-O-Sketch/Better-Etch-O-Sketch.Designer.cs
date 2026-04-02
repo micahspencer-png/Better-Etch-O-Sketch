f61aba57f2b00695f71ccfdc6b8367b6eb727508
micahspencer-png <micahspencer@isu.edu>
2026-04-02 14:04:50 -0600

    moved all items over from Past Etch-o-sketch program

--- Better-Etch-O-Sketch/Better-Etch-O-Sketch.Designer.cs
+++ Better-Etch-O-Sketch/Better-Etch-O-Sketch.Designer.cs
@@ -39,12 +39,27 @@
             this.StatusTimer = new System.Windows.Forms.Timer(this.components);
             this.AnalogTimer = new System.Windows.Forms.Timer(this.components);
             this.RefreshTimer = new System.Windows.Forms.Timer(this.components);
+            this.DisplayPictureBox = new System.Windows.Forms.PictureBox();
+            this.MouseRadioButton = new System.Windows.Forms.RadioButton();
+            this.ExternalRadioButton = new System.Windows.Forms.RadioButton();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.DrawGroupBox = new System.Windows.Forms.GroupBox();
+            this.ConnectGroupBox = new System.Windows.Forms.GroupBox();
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.WaveFormButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayPictureBox)).BeginInit();
+            this.DrawGroupBox.SuspendLayout();
+            this.ConnectGroupBox.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ExitButton
             // 
-            this.ExitButton.Location = new System.Drawing.Point(691, 370);
+            this.ExitButton.Location = new System.Drawing.Point(12, 116);
             this.ExitButton.Name = "ExitButton";
             this.ExitButton.Size = new System.Drawing.Size(100, 51);
             this.ExitButton.TabIndex = 0;
@@ -56,7 +71,7 @@
             // 
             this.PortComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PortComboBox.FormattingEnabled = true;
-            this.PortComboBox.Location = new System.Drawing.Point(682, 73);
+            this.PortComboBox.Location = new System.Drawing.Point(6, 21);
             this.PortComboBox.Name = "PortComboBox";
             this.PortComboBox.Size = new System.Drawing.Size(106, 33);
             this.PortComboBox.TabIndex = 1;
@@ -64,7 +79,7 @@
             // 
             // ConnectButton
             // 
-            this.ConnectButton.Location = new System.Drawing.Point(691, 314);
+            this.ConnectButton.Location = new System.Drawing.Point(12, 60);
             this.ConnectButton.Name = "ConnectButton";
             this.ConnectButton.Size = new System.Drawing.Size(100, 50);
             this.ConnectButton.TabIndex = 2;
@@ -79,9 +94,9 @@
             this.PortStatusLabel,
             this.Analog1StatusLabel,
             this.Analog2StatusLabel});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 424);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 507);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+            this.statusStrip1.Size = new System.Drawing.Size(948, 26);
             this.statusStrip1.TabIndex = 3;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -118,24 +133,133 @@
             // RefreshTimer
             // 
             this.RefreshTimer.Enabled = true;
-            this.RefreshTimer.Interval = 1000;
+            this.RefreshTimer.Interval = 2000;
             this.RefreshTimer.Tick += new System.EventHandler(this.RefreshTimer_Tick);
+            // 
+            // DisplayPictureBox
+            // 
+            this.DisplayPictureBox.Location = new System.Drawing.Point(12, 32);
+            this.DisplayPictureBox.Name = "DisplayPictureBox";
+            this.DisplayPictureBox.Size = new System.Drawing.Size(803, 472);
+            this.DisplayPictureBox.TabIndex = 4;
+            this.DisplayPictureBox.TabStop = false;
+            // 
+            // MouseRadioButton
+            // 
+            this.MouseRadioButton.AutoSize = true;
+            this.MouseRadioButton.Location = new System.Drawing.Point(6, 21);
+            this.MouseRadioButton.Name = "MouseRadioButton";
+            this.MouseRadioButton.Size = new System.Drawing.Size(69, 20);
+            this.MouseRadioButton.TabIndex = 5;
+            this.MouseRadioButton.TabStop = true;
+            this.MouseRadioButton.Text = "Mouse";
+            this.MouseRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ExternalRadioButton
+            // 
+            this.ExternalRadioButton.AutoSize = true;
+            this.ExternalRadioButton.Location = new System.Drawing.Point(6, 47);
+            this.ExternalRadioButton.Name = "ExternalRadioButton";
+            this.ExternalRadioButton.Size = new System.Drawing.Size(76, 20);
+            this.ExternalRadioButton.TabIndex = 5;
+            this.ExternalRadioButton.TabStop = true;
+            this.ExternalRadioButton.Text = "External";
+            this.ExternalRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // DrawGroupBox
+            // 
+            this.DrawGroupBox.Controls.Add(this.ClearButton);
+            this.DrawGroupBox.Controls.Add(this.WaveFormButton);
+            this.DrawGroupBox.Controls.Add(this.ColorButton);
+            this.DrawGroupBox.Controls.Add(this.MouseRadioButton);
+            this.DrawGroupBox.Controls.Add(this.ExternalRadioButton);
+            this.DrawGroupBox.Location = new System.Drawing.Point(820, 11);
+            this.DrawGroupBox.Name = "DrawGroupBox";
+            this.DrawGroupBox.Size = new System.Drawing.Size(116, 283);
+            this.DrawGroupBox.TabIndex = 6;
+            this.DrawGroupBox.TabStop = false;
+            // 
+            // ConnectGroupBox
+            // 
+            this.ConnectGroupBox.Controls.Add(this.PortComboBox);
+            this.ConnectGroupBox.Controls.Add(this.ConnectButton);
+            this.ConnectGroupBox.Controls.Add(this.ExitButton);
+            this.ConnectGroupBox.Location = new System.Drawing.Point(820, 333);
+            this.ConnectGroupBox.Name = "ConnectGroupBox";
+            this.ConnectGroupBox.Size = new System.Drawing.Size(124, 188);
+            this.ConnectGroupBox.TabIndex = 7;
+            this.ConnectGroupBox.TabStop = false;
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.Location = new System.Drawing.Point(13, 88);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(98, 55);
+            this.ColorButton.TabIndex = 6;
+            this.ColorButton.Text = "C&olor";
+            this.ColorButton.UseVisualStyleBackColor = true;
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // WaveFormButton
+            // 
+            this.WaveFormButton.Location = new System.Drawing.Point(12, 149);
+            this.WaveFormButton.Name = "WaveFormButton";
+            this.WaveFormButton.Size = new System.Drawing.Size(98, 55);
+            this.WaveFormButton.TabIndex = 6;
+            this.WaveFormButton.Text = "&Waveforms";
+            this.WaveFormButton.UseVisualStyleBackColor = true;
+            this.WaveFormButton.Click += new System.EventHandler(this.WaveFormButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(12, 210);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(98, 55);
+            this.ClearButton.TabIndex = 6;
+            this.ClearButton.Text = "C&lear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(948, 28);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(948, 533);
+            this.Controls.Add(this.ConnectGroupBox);
+            this.Controls.Add(this.DrawGroupBox);
+            this.Controls.Add(this.DisplayPictureBox);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.ConnectButton);
-            this.Controls.Add(this.PortComboBox);
-            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Better Etch-O-Sketch";
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayPictureBox)).EndInit();
+            this.DrawGroupBox.ResumeLayout(false);
+            this.DrawGroupBox.PerformLayout();
+            this.ConnectGroupBox.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,6 +277,17 @@
         private System.Windows.Forms.ToolStripStatusLabel Analog2StatusLabel;
         private System.Windows.Forms.Timer AnalogTimer;
         private System.Windows.Forms.Timer RefreshTimer;
+        private System.Windows.Forms.PictureBox DisplayPictureBox;
+        private System.Windows.Forms.RadioButton MouseRadioButton;
+        private System.Windows.Forms.RadioButton ExternalRadioButton;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.GroupBox DrawGroupBox;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button WaveFormButton;
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.GroupBox ConnectGroupBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
     }
 }
 
--- Better-Etch-O-Sketch/Better-Etch-O-Sketch.cs
+++ Better-Etch-O-Sketch/Better-Etch-O-Sketch.cs
@@ -22,7 +22,12 @@ namespace Better_Etch_O_Sketch
             //PortCombo();
             GetQYAtBoards();
         }
+        Color penColor = Color.Black;
+        int penWidth = 1;
 
+        bool isDrawing = false;
+        Point PreviousPoint;
+        bool trueClear = false;
         //Program Logic----------------------------------------------------------------------------------------------------------------
         string[] Ports;
         void PortCombo()
@@ -192,6 +197,159 @@ namespace Better_Etch_O_Sketch
             }
         }
 
+        private void SelectColor()
+        {
+            ColorDialog colorDialog1 = new ColorDialog();
+            colorDialog1.ShowDialog();
+            this.penColor = colorDialog1.Color;
+        }
+
+        private void DrawWaveform()
+        {
+            int s = 0;
+            int c = 1;
+            int t = 2;
+
+            Bitmap bmp = new Bitmap(DisplayPictureBox.Width, DisplayPictureBox.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+                g.Dispose();
+            }
+            DisplayPictureBox.Image = bmp;
+
+            Graphics grid = Graphics.FromImage(DisplayPictureBox.Image);
+            Pen thePen = new Pen(this.penColor, this.penWidth);
+            Pen Pen1 = new Pen(Color.Red, this.penWidth);
+            Pen Pen2 = new Pen(Color.Green, this.penWidth);
+            Pen Pen3 = new Pen(Color.Blue, this.penWidth);
+
+            for (int i = 1; i < 10; i++)
+            {
+                grid.DrawLine(thePen, i * 99, 0, i * 99, 550);
+                grid.DrawLine(thePen, 0, i * 56, 990, i * 56);
+                DisplayPictureBox.Invalidate();
+            }
+
+            WaveShape(s, Pen1);
+            WaveShape(c, Pen2);
+            WaveShape(t, Pen3);
+
+            grid.Dispose();
+            thePen.Dispose();
+        }
+
+        private void WaveShape(int waveShape, Pen draw)
+        {
+            int Amplitude = 275;
+            double Frequency = 0.00636;
+            int OffsetY = 280;
+            string Text = "";
+            int height = 0;
+            Color color = Color.White;
+
+            Graphics Wave = Graphics.FromImage(DisplayPictureBox.Image);
+
+            PointF[] points = new PointF[DisplayPictureBox.Width];
+            for (int x = 0; x < DisplayPictureBox.Width; x++)
+            {
+                double y = 0;
+                if (waveShape == 0)
+                {
+                    y = Amplitude * Math.Sin(Frequency * x);
+                    Text = "Sine Wave";
+                    height = 420;
+                    color = Color.Red;
+                }
+
+                if (waveShape == 1)
+                {
+                    y = Amplitude * Math.Cos(Frequency * x);
+                    Text = "Cosine Wave";
+                    height = 445;
+                    color = Color.Green;
+                }
+
+                if (waveShape == 2)
+                {
+                    y = Amplitude * Math.Tan(Frequency * x);
+                    Text = "Tangent Wave";
+                    height = 470;
+                    color = Color.Blue;
+                }
+
+                points[x] = new PointF(x, (float)(OffsetY - y));
+            }
+
+            Wave.DrawLines(draw, points);
+
+            Graphics g = Graphics.FromImage(DisplayPictureBox.Image);
+            Pen thePen = new Pen(Color.Cyan, 3);
+
+
+            Font drawFont = new Font("Times New Roman", 18);
+            SolidBrush drawBrush = new SolidBrush(color);
+
+            g.DrawString(Text, drawFont, drawBrush, 0, height);
+
+            g.Dispose();
+            drawBrush.Dispose();
+
+            draw.Dispose();
+            Wave.Dispose();
+        }
+        private void Sketch(Point XY)
+        {
+            Graphics g = Graphics.FromImage(DisplayPictureBox.Image);
+            Pen thePen = new Pen(this.penColor, this.penWidth);
+
+
+            g.DrawLine(thePen, PreviousPoint, XY);
+            PreviousPoint = XY;
+
+            DisplayPictureBox.Invalidate();
+            g.Dispose();
+            thePen.Dispose();
+        }
+
+        private void ClearDrawing()
+        {
+            if (trueClear == true)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    int xOffset = 10;
+                    int yOffset = 10;
+                    if (i == 1 || i == 3 || i == 5 || i == 7 || i == 9)
+                    {
+                        xOffset *= -1;
+                        yOffset *= -1;
+                    }
+                    int y = this.Top;
+                    int x = this.Left;
+                    int NewY = 0;
+                    int NewX = 0;
+
+                    NewY = y + yOffset;
+                    NewX = x + xOffset;
+
+                    this.Top = NewY;
+                    this.Left = NewX;
+
+                    System.Threading.Thread.Sleep(70);
+                }
+            }
+            Bitmap bmp = new Bitmap(DisplayPictureBox.Width, DisplayPictureBox.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+                g.Dispose();
+            }
+            DisplayPictureBox.Image = bmp;
+
+        }
+
+
         //Event Handler--------------------------------------------------------------------------------------------------------------
         private void ExitButton_Click(object sender, EventArgs e)
         {
@@ -231,5 +389,66 @@ namespace Better_Etch_O_Sketch
                 GetQYAtBoards();
             }
         }
+
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            SelectColor();
+        }
+
+        private void WaveFormButton_Click(object sender, EventArgs e)
+        {
+            DrawWaveform();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            trueClear = true;
+            ClearDrawing();
+        }
+
+        private void GraphicsDesign_MouseMove(object sender, MouseEventArgs e)
+        {
+
+
+            if (e.Button == MouseButtons.Middle)
+            {
+                SelectColor();
+            }
+
+        }
+        private void Picture_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            PreviousPoint = e.Location;
+        }
+
+        private void Picture_MouseMovement(object sender, MouseEventArgs e)
+        {
+            if (isDrawing == true)
+            {
+                Point XY = e.Location;
+
+                if (e.Button == MouseButtons.Left)
+                {
+                    Sketch(XY);
+                }
+            }
+            if (e.Button == MouseButtons.Middle)
+            {
+                SelectColor();
+            }
+        }
+
+        private void Picture_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Micah Spencer\n    RCET 2265\n    Fall 2025\n    Etch-O-Sketch Program\n    https://github.com/micahspencer-png/EtchOSketch.git");
+        }
     }
+}
 }
